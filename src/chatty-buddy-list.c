810193ebd64e300ff6a84bb849b749b56bf6fcb8
Guido Günther <agx@sigxcpu.org>
2018-10-11 14:45:52 +0200

    buddy-list: Handle all values in switch
    This is purely cosmetic

--- src/chatty-buddy-list.c
+++ src/chatty-buddy-list.c
@@ -944,6 +944,9 @@ chatty_blist_update (PurpleBuddyList *list,
     case PURPLE_BLIST_BUDDY_NODE:
       chatty_blist_update_buddy (list, node);
       break;
+    case PURPLE_BLIST_CHAT_NODE:
+    case PURPLE_BLIST_CONTACT_NODE:
+    case PURPLE_BLIST_GROUP_NODE:
     case PURPLE_BLIST_OTHER_NODE:
       return;
   }
