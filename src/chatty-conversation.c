68911dec04bc03eb3848ca7959fff04b8da41745
Andrea Schaefer <andrea.schaefer@puri.sm>
2020-02-19 18:47:41 +0100

    Fix #289 'don't show notifications in active chats'

--- src/chatty-conversation.c
+++ src/chatty-conversation.c
@@ -1999,6 +1999,7 @@ chatty_conv_write_conversation (PurpleConversation *conv,
                                 time_t              mtime)
 {
   ChattyConversation       *chatty_conv;
+  ChattyConversation       *active_chatty_conv;
   PurpleConversationType    type;
   PurpleConnection         *gc;
   PurplePluginProtocolInfo *prpl_info = NULL;
@@ -2007,8 +2008,11 @@ chatty_conv_write_conversation (PurpleConversation *conv,
   PurpleBlistNode          *node;
   g_autofree char          *color = NULL;
   gboolean                  group_chat = TRUE;
+  gboolean                  conv_active;
   GdkPixbuf                *avatar = NULL;
   GtkWidget                *icon = NULL;
+  ChattyWindow             *window;
+  GtkWidget                *convs_notebook;
   int                       chat_id;
   const char               *buddy_name;
   gchar                    *titel;
@@ -2115,7 +2119,17 @@ chatty_conv_write_conversation (PurpleConversation *conv,
                                    NULL,
                                    NULL);
     } else if (pcm.flags & PURPLE_MESSAGE_RECV) {
-      if (buddy && purple_blist_node_get_bool (node, "chatty-notifications")) {
+
+      window = chatty_utils_get_window ();
+
+      convs_notebook = chatty_window_get_convs_notebook (window);
+
+      active_chatty_conv = chatty_conv_container_get_active_chatty_conv (GTK_NOTEBOOK(convs_notebook));
+
+      conv_active = (chatty_conv == active_chatty_conv && gtk_widget_is_drawable (convs_notebook));
+
+      if (buddy && purple_blist_node_get_bool (node, "chatty-notifications") && !conv_active) {
+
         buddy_name = purple_buddy_get_alias (buddy);
 
         titel = g_strdup_printf (_("New message from %s"), buddy_name);
