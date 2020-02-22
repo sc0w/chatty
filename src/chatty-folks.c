33996bbb7e8b338fda10b34bf81a4811994880ad
Mohammed Sadiq <sadiq@sadiqpk.org>
2020-02-22 22:40:23 +0530

    folks: Remove unused code

--- src/chatty-folks.c
+++ src/chatty-folks.c
@@ -23,13 +23,6 @@
 #include <libebook-contacts/libebook-contacts.h>
 
 
-static chatty_folks_data_t chatty_folks_data;
-
-chatty_folks_data_t *chatty_get_folks_data (void)
-{
-  return &chatty_folks_data;
-}
-
 
 typedef struct {
   FolksIndividual  *individual;
@@ -198,125 +191,6 @@ chatty_folks_load_avatar (FolksIndividual  *individual,
 
     g_loadable_icon_load_async (avatar, size, NULL, cb_icon_load_async_ready, data);
   }
-}
-
-
-/**
- * chatty_folks_individual_has_phonenumber:
- * 
- * @individual:   a folks individual
- * @phone_number: a const char with the phone number
- * 
- * Check if phone_number matches a number in the
- * phone-details of the individual
- *
- * Returns: TRUE if there is a number match 
- * 
- */
-static gboolean
-chatty_folks_individual_has_phonenumber (FolksIndividual *individual,
-                                         const char      *phone_number)
-{
-  GeeSet            *phone_numbers;
-  GeeIterator       *iter;
-  FolksPhoneDetails *phone_details;
-  EPhoneNumberMatch  number_match;
-  gboolean           result = FALSE;
-  GError            *error = NULL;
-
-
-  g_return_val_if_fail (FOLKS_IS_INDIVIDUAL (individual), FALSE);
-
-  phone_details = FOLKS_PHONE_DETAILS (individual);
-  phone_numbers = folks_phone_details_get_phone_numbers (phone_details);
-  iter = gee_iterable_iterator (GEE_ITERABLE(phone_numbers));
-
-  while (gee_iterator_next (iter)) {
-    FolksPhoneFieldDetails *field_details;
-    char                   *number;
-
-    field_details = gee_iterator_get (iter);
-    number = folks_phone_field_details_get_normalised (field_details);
-
-    number_match = e_phone_number_compare_strings (phone_number, number, &error);
-
-    if (error != NULL) {
-      g_debug ("Error comparing numbers: %s", error->message);
-      g_clear_error (&error);
-
-      result = strcmp (phone_number, number) == 0;
-
-    } else if (number_match == E_PHONE_NUMBER_MATCH_EXACT ||
-               number_match == E_PHONE_NUMBER_MATCH_NATIONAL) {
-          
-      result = TRUE;
-    }
-
-    g_free (number);
-  }
-
-  return result;
-}
-
-
-/**
- * chatty_folks_has_individual_with_phonenumber:
- * 
- * @number: a const char with the phone number
- * 
- * Lookup a folks individual by phone number
- *
- * Returns: the id of the individual or NULL
- * 
- */
-const char *
-chatty_folks_has_individual_with_phonenumber (const char *number) 
-{ 
-  FolksIndividual *individual;
-  GeeMapIterator  *iter;
-
-  chatty_folks_data_t *chatty_folks = chatty_get_folks_data ();
-
-  if (chatty_folks->individuals == NULL) {
-    return NULL;
-  }
-
-  iter = gee_map_map_iterator (chatty_folks->individuals);
-
-  while (gee_map_iterator_next (iter)) {
-    individual = gee_map_iterator_get_value (iter);
-
-    if (chatty_folks_individual_has_phonenumber (individual, number)) {
-      return gee_map_iterator_get_key (iter);
-    }
-  }
-
-  return NULL;
-}
-
-/**
- * chatty_folks_get_individual_name_by_id:
- * 
- * @id: a const char with the ID of an individual
- * 
- * Get the name of an individual by its ID
- *
- * Returns: the name of the individual or NULL
- */
-const char *
-chatty_folks_get_individual_name_by_id (const char *id) 
-{ 
-  FolksIndividual *individual;
-
-  chatty_folks_data_t *chatty_folks = chatty_get_folks_data ();
-
-  individual = FOLKS_INDIVIDUAL(gee_map_get (chatty_folks->individuals, id));
-
-  if (individual != NULL) {
-    return folks_individual_get_display_name (individual);
-  }
-
-  return NULL;
 }
 
 
