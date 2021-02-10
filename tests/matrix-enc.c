a5fde81a751598a49de75df8fff4ba8b592429a3
Mohammed Sadiq <sadiq@sadiqpk.org>
2021-02-10 17:13:28 +0530

    tests: Fix matrix-enc test to work with olm3
    Fixes #471

--- tests/matrix-enc.c
+++ tests/matrix-enc.c
@@ -19,15 +19,17 @@
 #include <olm/olm.h>
 #include <sys/random.h>
 
+#include "chatty-config.h"
+
 #include "matrix/matrix-utils.h"
 #include "matrix/matrix-enc.h"
 
 typedef struct EncData {
-  MatrixEnc *enc;
   char *user_id;
   char *device_id;
   char *pickle_key;
-  char *pickle;
+  char *olm2_pickle;
+  char *olm3_pickle;
   char *curve_key;
   char *ed_key;
   /* These are not marked as published */
@@ -38,11 +40,11 @@ typedef struct EncData {
 
 EncData enc[] = {
   {
-    NULL,
     "@neo:example.com",
     "JOJOAREBZY",
     "cefdef40-3b16-4d71-8685-2740833c3297",
     "HoxM0prMq/t3pGK6OLSet1h/1H8bnILjY1fp2Colr/ATOFevqulWPpiyktCJ0Cw1KZGQvHzHI481s2n2xu7yJkVC4TxrX5gKNTQ2QevZpVKD4PEmRaq40twVWOBjuAoxch16LCZs7CjNCYGtR7vhO2M6s5YchJhMFXJmH0Seik+yP5vyPJDx7nLcS6PZWEYGtr5U3VFUf4pj453NxrX7bAy7HRQOY642Vqkd1F06w6367naS0/0cgMj1aWDSK+z+a+F7TNfOaKky+90hLTYtkyM8VbFx8e6BmrmTyheCxpGKC1JQnj0QGCKb8cxAgV3FIaol8lOoFngA1Q7m6aEvkh3PyEqCUHqXlt70E0zHZy021tavDG2g1+GaCar9hOPayfVNpPZdFrL+avUJnTQ6DnxiGcBpobv+JiBjdOMb7rpG80P9nkPwre1D+JTvbCKgI+kZHUX3+Xs6YX01JJ8gmArl9GT1k4WkDi3iR74AAynXAASJRuwD6oKUbt/R5Ey3eqcmMh1IrwI",
+    "uugdtov1PcB8ZVO+OqXztUUImU/22WTIMYZnvzzCuTmHFLZkUtfha8QSMaoiyL0UGZSAmiaN4lF2k4+QtwYtdUBWUaWlOZd4ZTsacs7L6PkqpZvo491a6SYzHs/kNt/K23/x4BM7H/GBM2u0UOeekep+9IkI6d/j2vYSFYAIPj9nWQRTIfO9jCtwu1XcgzG/HcH5PRwq3F/Xq92VGrgv+AftnHH8XR1cK8OPkkL+Rm6J5VtfZgS3ygMOLy4j12+8Wo+BA8zxbBO8RKgD1c9doGX+MpYB7hgfkI+3S2Z2RTZq363mn+/sCu84ZXmkMfc+tvPS9W+65OugYCIm3sQtnM2Rz9xnFbZ+cJ4uX//3JCUmZJhvoSiPfMGu6XjU3HYsBlNViVMUYlNFpWCONCf+fiV2XjxOVrTZ3+8kPsKasZ7awWr3FLI/HhN0Wc7Wt8vTJwygV5654rR3dwvawhZpz+48J6hGBmUPiEKCDQZPfttOZhgo1bOiCPXgTI80/c1R7WxdDZVVOGopvuFDxwSn+ToalT81x0jVddOApLDpQtONB7NBfRqzK8nnLPmsj8a50Ljpv4G2gciYzZ3gO/ZRBrm5N5H90Vyf6AYR3PAWyJX8rnkSpVTu8rMhkGCIxvhkdc/HzcQUD1NlLTwlJxIxdDn0B33kju3jscDRvHgEWDy0Q2J5aDJTcf2ih13tVo530dSJXheEMeA",
     "UpcUBGKMHofzYsMwU5yBM79kL8dAMYXuPav+V1hZnkc",
     "w33QBdjIgxJtP8jnFVCdAgjuCXy0XXT5eIPHJwvmbJw",
     "YbFN9bWf0dfQCbsxsx5Lo779zXKAD/ctCN4qEj8G0T8",
@@ -50,11 +52,11 @@ EncData enc[] = {
     "OeS+WTn5e8k6C1jH4Mw1Otl3of0t+jPvWh2GusmlfBY",
   },
   {
-    NULL,
     "@neo:example.org",
     "JDFVAREDFE",
     "bc80e9a7-f175-4352-9b07-7178b63546ff",
     "GeRh9PcLuV4diHtRBPngRcVVz54++xGLCvYuHKD0kMCpKHejUodLlpxCfI3Ax25E3nAqN7/k++BWywscZRyNqxVJ0J/w46RgiBrAm2+K3u1ylpSxREf0DwYGXSlMBEA7En5FdZkpcGcM83CiSs/3IqmTOMfMq66u7Uoe8wQ9ZJ6KiYMpv9lswkS0xypC0LnEQQWY8A57QKg9zqXhKvQPwQ1AhxaxWGxYaLQ5yYJZFwd+on3mpK+j7Q4BlM1fqA6hrTTBJ/UldBd8Algc5O6UIpB2Xp31ZDmLb9/D76P2/WD12I61x2T1V6WgmNneKRCXTN4PkPCbk+06QXAjL6OAKoK9TH17Hdu2uNGc83ye5reK1ZSfiHklU5NeyCI/ttJn/Pa0I6VnfhcgisxL+TgsXbgqHmi7GD6tvRaeVVr3x9QCOPyGFehaLF20TUZDm8EtbjupMbCTFWb5LU07yT4wM/mU/OsHVgJ1gQCCHrdGuZomyS9XZnkVLGQo+kT+VnUOeTmLznSROV0",
+    "DvyR7WJRCvvj1DaM7lJmCcnOUTHL3fPUJwgXjt8euKRO0JGY7pN4anP/z2BavTCLNpzKmXkkOYuBgQiEKU5jT8He/Meadx4udZrlxyacoEXt8PJH3spIoM8NaycuvYTwIVTvvvBVlnYME5M/Wwy0xd5cmJsAHth6mHTLnk5Hn1+i6fbxvLPvDmpm8D16X7z+t8eXEEoPckcjsWkXjoOFKHFGmqc5+K4gRt22xMJh0inZamzIb6sXy2Ov5h0d2V1gfOjYwdObKRjA/yTZQ1N5iR430H3jFuVl+BfG9VtL0EQH/w/N6q4C8wAVPwNDvsLEoII3uOtn/+ARKR7AJGhwzfq8k8WWEx1joWTKjKXb3Nm6RlLT/u4K3qVdSquOUV2efKEFhFAz8bH5D1T+XyXOPHgVIpDFMmwgnFxKCGFPqQrv6qeFrrmmHPxPNgmcNjiplDW1lTYr2TjOughLJbQpR/bVGI+9r0PMZHI0g18WgBGsf4Kv8t3x0K+Ui6ZJWL4eYhkvVRFw9kHew/nIisqBAqU4CpWlXips+52xoVXOQXTvtAzArmY5YLg2GxqlidEuRstK+oydg1NCfzqfcBNLu4ux5mNatgBLOCCrt6VIpM0FENZvihPku6/NA+YMpi0PuT2lsaOc94NE/AhDvuMx4z09dPWkywNK/d1yecmRbcdTHYoeZ51NS309OBD1KnoUT4NS6bBV0xQ",
     "FMXixHL3B/8xYAOs6MK4EeghAXZppZgNjb8o9UK4NSg",
     "mnfxCgqI0trldd23vrz1SGbqQDW8TXcKKRlo9M3Mj64",
     "d9lE1FD5s9I0gT4kT5jgQYDSDvNKQuBAQM/5LaSVtms",
@@ -67,17 +69,22 @@ EncData enc[] = {
 static void
 test_matrix_enc_verify (void)
 {
+  g_autoptr(MatrixEnc) enc1 = NULL;
+  g_autoptr(MatrixEnc) enc2 = NULL;
   JsonObject *object, *root;
   char *sign, *json, *key_label;
   const char *message;
   EncData data;
 
   data = enc[0];
-  g_assert (MATRIX_IS_ENC (data.enc));
+  enc1 = matrix_enc_new (NULL, enc[0].olm2_pickle, enc[0].pickle_key);
+  enc2 = matrix_enc_new (NULL, enc[1].olm2_pickle, enc[1].pickle_key);
+  g_assert (MATRIX_IS_ENC (enc1));
+  g_assert (MATRIX_IS_ENC (enc2));
 
   /* @message is in canonical form */
   message = "{\"timeout\":20000,\"type\":\"m.message\"}";
-  sign = matrix_enc_sign_string (data.enc, message, -1);
+  sign = matrix_enc_sign_string (enc1, message, -1);
   g_assert_nonnull (sign);
 
   root = matrix_utils_string_to_json_object (message);
@@ -99,13 +106,14 @@ test_matrix_enc_verify (void)
 
   root = matrix_utils_string_to_json_object (json);
   g_assert_nonnull (root);
-  g_assert_true (matrix_enc_verify (data.enc, root, data.user_id,
+
+  g_assert_true (matrix_enc_verify (enc1, root, data.user_id,
                                     data.device_id, data.ed_key));
-  g_assert_true (matrix_enc_verify (enc[1].enc, root, data.user_id,
+  g_assert_true (matrix_enc_verify (enc2, root, data.user_id,
                                     data.device_id, data.ed_key));
-  g_assert_false (matrix_enc_verify (data.enc, root, data.user_id,
+  g_assert_false (matrix_enc_verify (enc1, root, data.user_id,
                                     data.device_id, data.curve_key));
-  g_assert_false (matrix_enc_verify (data.enc, root, enc[1].user_id,
+  g_assert_false (matrix_enc_verify (enc1, root, enc[1].user_id,
                                     data.device_id, data.ed_key));
   json_object_unref (root);
   g_free (json);
@@ -119,17 +127,35 @@ test_matrix_enc_new (void)
   EncData data;
 
   for (guint i = 0; i < G_N_ELEMENTS (enc); i++) {
-    data = enc[i];
-    g_assert (MATRIX_IS_ENC (data.enc));
+    g_autoptr(MatrixEnc) matrix_enc = NULL;
 
-    value = matrix_enc_get_curve25519_key (data.enc);
+    data = enc[i];
+
+#ifdef HAVE_OLM3
+    matrix_enc = matrix_enc_new (NULL, data.olm3_pickle, data.pickle_key);
+    g_assert (MATRIX_IS_ENC (matrix_enc));
+
+    pickle = matrix_enc_get_account_pickle (matrix_enc);
+    g_assert_cmpstr (pickle, ==, data.olm3_pickle);
+    g_clear_pointer (&pickle, g_free);
+    g_object_unref (matrix_enc);
+#endif
+
+    matrix_enc = matrix_enc_new (NULL, data.olm2_pickle, data.pickle_key);
+    g_assert (MATRIX_IS_ENC (matrix_enc));
+
+    value = matrix_enc_get_curve25519_key (matrix_enc);
     g_assert_cmpstr (value, ==, data.curve_key);
 
-    value = matrix_enc_get_ed25519_key (data.enc);
+    value = matrix_enc_get_ed25519_key (matrix_enc);
     g_assert_cmpstr (value, ==, data.ed_key);
 
-    pickle = matrix_enc_get_account_pickle (data.enc);
-    g_assert_cmpstr (pickle, ==, data.pickle);
+    pickle = matrix_enc_get_account_pickle (matrix_enc);
+#ifdef HAVE_OLM3
+    g_assert_cmpstr (pickle, ==, data.olm3_pickle);
+#else
+    g_assert_cmpstr (pickle, ==, data.olm2_pickle);
+#endif
     g_clear_pointer (&pickle, g_free);
   }
 }
@@ -138,18 +164,6 @@ int
 main (int   argc,
       char *argv[])
 {
-  EncData *data;
-
-  data = &enc[0];
-  data->enc = matrix_enc_new (NULL, data->pickle, data->pickle_key);
-  g_assert (MATRIX_IS_ENC (data->enc));
-  matrix_enc_set_details (data->enc, data->user_id, data->device_id);
-
-  data = &enc[1];
-  data->enc = matrix_enc_new (NULL, data->pickle, data->pickle_key);
-  g_assert (MATRIX_IS_ENC (data->enc));
-  matrix_enc_set_details (data->enc, data->user_id, data->device_id);
-
   g_test_init (&argc, &argv, NULL);
 
   g_test_add_func ("/matrix/enc/new", test_matrix_enc_new);
