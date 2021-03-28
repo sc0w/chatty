f265beb4a08c17aeb6a423e5222d0875fbe0f731
Mohammed Sadiq <sadiq@sadiqpk.org>
2021-03-28 19:16:29 +0530

    matrix-enc: Remove unused code
    MaOlmOutSession is only set and never used

--- src/matrix/matrix-enc.c
+++ src/matrix/matrix-enc.c
@@ -69,13 +69,6 @@ struct _MatrixEnc
 
 G_DEFINE_TYPE (MatrixEnc, matrix_enc, G_TYPE_OBJECT)
 
-typedef struct _MaOlmOutSession {
-  OlmSession *session;
-  char *session_id;
-  char *sender_id;
-  char *device_id;
-} MaOlmOutSession;
-
 static void
 free_olm_session (gpointer data)
 {
@@ -1141,7 +1134,6 @@ matrix_enc_create_out_group_keys (MatrixEnc  *self,
   g_autofree uint8_t *session_id = NULL;
   g_autofree uint8_t *random = NULL;
   JsonObject *root, *child;
-  MaOlmOutSession *out_session;
   BuddyDevice *device;
   size_t length;
   size_t error;
@@ -1273,9 +1265,6 @@ matrix_enc_create_out_group_keys (MatrixEnc  *self,
         json_object_set_int_member (content, "type", olm_encrypt_message_type (olm_session));
         json_object_set_string_member (content, "body", encrypted);
       }
-
-      out_session = g_new0 (MaOlmOutSession, 1);
-      out_session->session = g_steal_pointer (&olm_session);
     }
   }
 
