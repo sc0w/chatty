525b9f172039a2bbd44acb016f1fb9499dd1ed61
Mohammed Sadiq <sadiq@sadiqpk.org>
2021-04-12 18:44:49 +0530

    manager: Remove no longer used code

--- src/chatty-manager.c
+++ src/chatty-manager.c
@@ -2531,56 +2531,6 @@ chatty_manager_add_chat (ChattyManager *self,
   return item ? item : chat;
 }
 
-/**
- * chatty_conv_join_chat:
- * @chat: a PurpleChat
- *
- * Joins a group chat
- * If there is already an instance of the chat
- * the GUI presents it to the user.
- *
- */
-void
-chatty_conv_join_chat (PurpleChat *chat)
-{
-  PurpleAccount            *account;
-  PurpleConversation       *conv;
-  PurplePluginProtocolInfo *prpl_info;
-  GHashTable               *components;
-  const char               *name;
-  char                     *chat_name;
-
-  account = purple_chat_get_account(chat);
-  prpl_info = PURPLE_PLUGIN_PROTOCOL_INFO(purple_find_prpl (purple_account_get_protocol_id (account)));
-
-  components = purple_chat_get_components (chat);
-
-  if (prpl_info && prpl_info->get_chat_name) {
-    chat_name = prpl_info->get_chat_name(components);
-  } else {
-    chat_name = NULL;
-  }
-
-  if (chat_name) {
-    name = chat_name;
-  } else {
-    name = purple_chat_get_name(chat);
-  }
-
-  conv = purple_find_conversation_with_account (PURPLE_CONV_TYPE_CHAT,
-                                                name,
-                                                account);
-
-  if (!conv || purple_conv_chat_has_left (PURPLE_CONV_CHAT(conv))) {
-    chatty_conv_add_history_since_component(components, account->username, name);
-    serv_join_chat (purple_account_get_connection (account), components);
-  } else if (conv) {
-    purple_conversation_present(conv);
-  }
-
-  g_free (chat_name);
-}
-
 gboolean
 chatty_manager_set_uri (ChattyManager *self,
                         const char    *uri)
--- src/chatty-manager.h
+++ src/chatty-manager.h
@@ -75,7 +75,6 @@ gboolean        chatty_manager_save_account_finish    (ChattyManager      *self,
                                                        GError            **error);
 ChattyChat     *chatty_manager_add_chat               (ChattyManager      *self,
                                                        ChattyChat         *chat);
-void            chatty_conv_join_chat                 (PurpleChat         *chat);
 gboolean        chatty_manager_set_uri                (ChattyManager      *self,
                                                        const char         *uri);
 ChattyHistory  *chatty_manager_get_history            (ChattyManager      *self);
