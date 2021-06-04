4634257583f7e8cb76328ae83d508ad34fa0c07f
Mohammed Sadiq <sadiq@sadiqpk.org>
2021-06-04 20:42:46 +0530

    window: Align function declarations

--- src/chatty-window.h
+++ src/chatty-window.h
@@ -19,12 +19,11 @@ G_BEGIN_DECLS
 
 G_DECLARE_FINAL_TYPE (ChattyWindow, chatty_window, CHATTY, WINDOW, GtkApplicationWindow)
 
-GtkWidget *chatty_window_new     (GtkApplication *application);
-void       chatty_window_set_uri (ChattyWindow *self,
-                                 const char   *uri);
-ChattyChat *chatty_window_get_active_chat (ChattyWindow *self);
-void        chatty_window_open_chat       (ChattyWindow *self,
-                                           ChattyChat   *chat);
-
+GtkWidget  *chatty_window_new             (GtkApplication *application);
+void        chatty_window_set_uri         (ChattyWindow   *self,
+                                           const char     *uri);
+ChattyChat *chatty_window_get_active_chat (ChattyWindow   *self);
+void        chatty_window_open_chat       (ChattyWindow   *self,
+                                           ChattyChat     *chat);
 
 G_END_DECLS
